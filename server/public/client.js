7be914ea61857857247096a01bd555531c4f379b
Anders Boyum <Acboyum@gmail.com>
2023-04-14 20:42:21 -0500

    accidentally created and finished both POST route and DELETE route. Whoops.

--- server/public/client.js
+++ server/public/client.js
@@ -7,6 +7,9 @@ function onReady() {
 
     // Function to POST new task to the server
     $('#inputFields').on('click', '#submitButton', createNewTask);
+
+    // Function to DELETE task from DOM/DB
+    $('#twoLists').on('click', ".deleteButton", deleteTask);
 };
 
 // Function to retrieve current task info from tasks server and render to DOM
@@ -17,6 +20,9 @@ function getAndRenderTasks() {
         url: '/tasks'
     }).then(function (response) {
         console.log('In GET /tasks, retrieved tasks', response);
+        // clear div fields for re-render
+        $('#taskList').empty();
+        $('#completedTaskList').empty();
         // calling renderFunction to keep things cleaner
         renderTasks(response)
     }).catch(function(error) {
@@ -30,7 +36,7 @@ function renderTasks(array) {
         if (task.isComplete === false) {
             $('#taskList').append(`
                 <li class="tasks" data-id=${task.id}> 
-                ${task.task} ${task.isComplete} ${task.completeBy} ${task.notes} 
+                ${task.task} || ${task.completeBy} || ${task.notes} 
                 <button class="completeButton">✅</button>
                 <button class="deleteButton">X</button>
                 </li>
@@ -39,7 +45,7 @@ function renderTasks(array) {
         else if(task.isComplete === true) {
             $('#completedTaskList').append(`
                 <li data-id="${task.id}>
-                ${task.task} || ${task.isComplete} || ${task.completeBy} || ${task.notes} 
+                ${task.task} || ${task.completeBy} || ${task.notes} 
                 <button class="completedButton">Completed</button>
                 <button class="deleteButton">X</button>
                 </li>
@@ -56,4 +62,43 @@ function createNewTask(event) {
     let newTask = $('#taskInput').val();
     let completeBy = $('#taskCompleteBy').val();
     let taskNotes = $('#taskNotes').val();
+
+    $.ajax({
+        method: 'POST',
+        url: '/tasks',
+        data: {
+            task: newTask,
+            completeBy: completeBy,
+            isComplete: false,
+            notes: taskNotes
+        }
+    }).then(function(response) {
+        console.log('Successfully created new task');
+        // clear input fields upon success
+        $('#taskInput').val('');
+        $('#taskCompleteBy').val('');
+        $('#taskNotes').val('');
+        // re-render tasks from db
+        getAndRenderTasks();
+    }).catch(function(error) {
+        console.log("Encountered error creating new task", error);
+    });
+}//end createNewTask function
+
+
+// Function needs to send DELETE SQL query to db, then call getAndRenderTasks to update DOM
+function deleteTask() {
+    // get ID of task to delete
+    let taskToDelete = $(this).parent().data('id');
+
+    // make DELETE req to server for the task with ID of ${taskToDelete}
+    $.ajax({
+        method: 'DELETE',
+        url: `/tasks/${taskToDelete}`
+    }).then(function(response) {
+        console.log("Successfully deleted task! Though I suppose you knew that since it's gone.");
+        getAndRenderTasks();
+    }).catch(function(error) {
+        console.log("Can't get out of doing your tasks that easily!! Kidding, there was an error in deleting.")
+    })
 }
