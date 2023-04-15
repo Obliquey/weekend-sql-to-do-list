21c6731c1aa4ee469d8938e49c3b8fdb8492f1e9
Anders Boyum <Acboyum@gmail.com>
2023-04-14 20:00:44 -0500

    beginning to add POST route for new Task. Created input fields + form in HTML

--- server/public/client.js
+++ server/public/client.js
@@ -2,9 +2,14 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log("We're Live!");
+    // Function to keep DOM constantly updated with freshest info
     getAndRenderTasks();
+
+    // Function to POST new task to the server
+    $('#inputFields').on('click', '#submitButton', createNewTask);
 };
 
+// Function to retrieve current task info from tasks server and render to DOM
 function getAndRenderTasks() {
     
     $.ajax({
@@ -12,12 +17,14 @@ function getAndRenderTasks() {
         url: '/tasks'
     }).then(function (response) {
         console.log('In GET /tasks, retrieved tasks', response);
+        // calling renderFunction to keep things cleaner
         renderTasks(response)
     }).catch(function(error) {
         console.log('Could not retrieve tasks in GET /tasks', error);
     });
-}
+}//end getAndRenderTasks function
 
+// Function to render tasks to DOM (mainly to keep code clean)
 function renderTasks(array) {
     for (let task of array) {
         if (task.isComplete === false) {
@@ -39,4 +46,14 @@ function renderTasks(array) {
             `);
         }
     }
+}//end renderTasks function
+
+
+// Function for retrieving new task info, bundling into object, and POSTing to server
+function createNewTask(event) {
+    event.preventDefault();
+
+    let newTask = $('#taskInput').val();
+    let completeBy = $('#taskCompleteBy').val();
+    let taskNotes = $('#taskNotes').val();
 }
